b447546b2aaf4dd96631dc88663ec9795055a247
Duuckee <charley.lonergan@iinet.net.au>
2026-08-04 20:16:37 +1000

    Update (idk naming)

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'loggr-app-v3'
+const CACHE_NAME = 'loggr-app-v4'
 const APP_SHELL = ['/', '/index.html', '/manifest.webmanifest']
 
 self.addEventListener('install', (event) => {
